00ec732bf5369b1e973f429906fedd9992d42f16
Latik <latik@bistracker.org.ua>
2012-03-08 00:58:08 +0200

    Init first env for future

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php 
+// SET PHP ENVIRONMENT
+@error_reporting(E_ALL | E_STRICT);
+@ini_set('error_reporting', E_ALL | E_STRICT);
+@ini_set('display_errors', '1');
+@ini_set('display_startup_errors', '1');
+@ini_set('ignore_repeated_errors', '1');
+date_default_timezone_set('UTC');
+
+// use composer autoload class
+require_once __DIR__.'/../vendor/.composer/autoload.php';
+
+?>

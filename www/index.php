eb8eac46d1cb5b19be46d7ed183e03959df05114
Latik <latik@bistracker.org.ua>
2012-07-03 22:42:07 +0300

    Update autoload path for new ver composer

--- www/index.php
+++ www/index.php
@@ -1,10 +1,10 @@
 <?php
 // SET PHP DEVEL ENVIRONMENT
-@error_reporting(E_ALL | E_STRICT);
-@ini_set('error_reporting', E_ALL | E_STRICT);
-@ini_set('display_errors', '1');
-@ini_set('display_startup_errors', '1');
-@ini_set('ignore_repeated_errors', '1');
+@error_reporting(-1);
+@ini_set('error_reporting', -1);
+@ini_set('display_errors', 1);
+@ini_set('display_startup_errors', 1);
+@ini_set('ignore_repeated_errors', 1);
 
 // Default timezone of server
 date_default_timezone_set('UTC');
@@ -13,7 +13,7 @@ date_default_timezone_set('UTC');
 define ('DOCROOT', str_replace('www', '', realpath(__DIR__)));
 define ('APPPATH', DOCROOT . 'app'. DIRECTORY_SEPARATOR );
 
-require_once __DIR__.'/../vendor/.composer/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Run engine
 $app = new Framework\Application;
